aad8b29e05b2cd456e7ecd53a2a5552b4409e59e
Ahmad Ganjtabesh <ifireir@gmail.com>
2024-02-28 14:16:38 +0330

    feat: Add `HashFamily` implementing `xxHash64` (#7)
    * feat: Add `HashFamily` interface
    
    * feat: Add `HashFamily` implementing `xxHash64`

---
+++ internal/hash_family/xxhash64_hash_family.go
@@ -0,0 +1,48 @@
+package hash_family
+
+import (
+	"hash"
+
+	"github.com/agtabesh/lsh/internal/types"
+	"github.com/pierrec/xxHash/xxHash64"
+)
+
+type XXHASH64HashFamily struct {
+	hasher []hash.Hash64
+	count  int
+}
+
+// NewXXHASH64HashFamily initializes a new HashFamily with the specified number of hash functions.
+func NewXXHASH64HashFamily(count int) XXHASH64HashFamily {
+	hasher := make([]hash.Hash64, count)
+	for i := 0; i < count; i += 1 {
+		hasher[i] = xxHash64.New(uint64(i)) // Initialize each hash function with a unique seed
+	}
+	return XXHASH64HashFamily{
+		hasher: hasher,
+		count:  count,
+	}
+}
+
+// Hash computes the hash values of a given string using all hash functions in the family.
+func (hf XXHASH64HashFamily) Hash(s string) types.Signature {
+	hashes := make(types.Signature, hf.count)
+	for i := 0; i < hf.count; i += 1 {
+		b := []byte(s)
+		hf.hasher[i].Reset()
+		hf.hasher[i].Write(b)
+		h := hf.hasher[i].Sum64()
+		hashes[i] = types.SignatureEntry(h)
+	}
+	return hashes
+}
+
+// MinHash computes the min hash values of a given vector using all hash functions in the family.
+func (hf XXHASH64HashFamily) MinHash(vector types.Vector) types.Signature {
+	var signature types.Signature
+	for k := range vector {
+		newSignature := hf.Hash(k.String())
+		signature = signature.FindMin(newSignature)
+	}
+	return signature
+}
